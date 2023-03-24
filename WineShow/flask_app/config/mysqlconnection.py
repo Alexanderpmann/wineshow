9838cbad7d0c396a33de9dead33e485f0569ecb0
Alexanderpmann <99506481+Alexanderpmann@users.noreply.github.com>
2023-03-23 22:45:32 -0400

    Initial deployment

--- WineShow/flask_app/config/mysqlconnection.py
+++ WineShow/flask_app/config/mysqlconnection.py
@@ -5,9 +5,9 @@ import pymysql.cursors
 # this class will give us an instance of a connection to our database
 class MySQLConnection:
     def __init__(self, db):
-        connection = pymysql.connect(host = 'localhost',
-                                    user = 'root', # change the user and password as needed
-                                    password = 'root', 
+        connection = pymysql.connect(host = 'kfgk8u2ogtoylkq9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+                                    user = 'c8m5mcw2rb4wlfvv', # change the user and password as needed
+                                    password = 'lvi367nubot669au', 
                                     db = db,
                                     charset = 'utf8mb4',
                                     cursorclass = pymysql.cursors.DictCursor,
